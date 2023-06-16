22c3a0e4e37abbf2f0c3c0cd6f9c5c355a57fbea
Anson Horton <anson.horton@gmail.com>
2023-06-16 13:22:31 -0700

    Add color to prompts

--- src/main/java/com/github/ikeras/App.java
+++ src/main/java/com/github/ikeras/App.java
@@ -1,16 +1,25 @@
 package com.github.ikeras;
 
 import java.util.Scanner;
+
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+
 import java.util.Map;
 import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("This a math game where the goal is to create an expression using 5 provided numbers");
-        System.out.println("that evaluates to the goal number. For example, if the goal number is 13 and the");
-        System.out.println("provided numbers are 2, 6, 4, 5, and 6 then a correct answer would be: 2 + 6 - 5 + 4 + 6.");
-        System.out.println("The only operators that may be used are +, -, *, /. Order of operations apply but can be");
-        System.out.println("overridden using parantheses. Good luck and have fun!\n");
+        AnsiConsole.systemInstall();
+
+        System.out.println(ansi().fgBrightBlue().a("=========================================================================================").reset());
+        System.out.println(ansi().fgBrightBlue().a("""            
+            This a math game where the goal is to create an expression using 5 provided numbers
+            that evaluates to the goal number. For example, if the goal number is 13 and the
+            provided numbers are 2, 6, 4, 5, and 6 then a correct answer would be: 2 + 6 - 5 + 4 + 6.
+            The only operators that may be used are +, -, *, /. Order of operations apply but can be
+            overridden using parantheses. Good luck and have fun!""").reset());
+        System.out.println(ansi().fgBrightBlue().a("=========================================================================================\n").reset());
 
         try (Scanner scanner = new Scanner(System.in)) {
             String userInput = "";
@@ -21,7 +30,7 @@ public class App {
                 count++;
                 Problem problem = new Problem(5);
                 System.out.println("Problem #" + count);
-                System.out.println("Goal: " + problem.getResult());
+                System.out.println(ansi().fgBrightYellow().a("Goal: " + problem.getResult()).reset());
                 System.out.print("Operands: ");
                 for (int operand : problem.getOperands()) {
                     System.out.print(operand + " ");
@@ -50,30 +59,43 @@ public class App {
                     if (operandCounts.equals(expressionOperandCounts)) {
                         if (expression.getResult() == problem.getResult()) {
                             solved++;
-                            System.out.println("Excellent job! You have solved this correctly!");
+                            printSuccess("Excellent job! You have solved this correctly!");
                         } else {
-                            System.out.println("Unfortunately, the expected result was " + problem.getResult() + " and your result was " + expression.getResult());
-                            System.out.println("A possible solution would have been " + problem.getCalculatedExpression());
+                            printError("Unfortunately, the expected result was " + problem.getResult() + " and your result was " + expression.getResult());
+                            printError("A possible solution would have been " + problem.getCalculatedExpression());
                         }
                     } else {
-                        System.out.print("Unfortunately, you did not use the correct operands. Expected: ");
+                        printError("Unfortunately, you did not use the correct operands. Expected: ", false);
                         for (int operand : problem.getOperands()) {
-                            System.out.print(operand + " ");
+                            printError(operand + " ", false);
                         }
-                        System.out.print("Used: ");
+                        printError("Used: ", false);
                         for (int operand : expression.getOperands()) {
-                            System.out.print(operand + " ");
+                            printError(operand + " ", false);
                         }
                         System.out.println();
                     }
                 } catch (IllegalArgumentException err) {
-                    System.out.println("Unfortunately your expression guess resulted in an error: " + err.getMessage() + ".");
+                    printError("Unfortunately your expression guess resulted in an error: " + err.getMessage() + ".");
                 }
 
                 System.out.println();
             }
 
-            System.out.println("You solved " + solved + " out of " + count + " problems!");
+            printSuccess("You solved " + solved + " out of " + (count - 1) + " problems!");
         }
+    }
+
+    private static void printError(String message, boolean newline) {
+        if (newline) message += "\n";
+        System.out.print(ansi().fgBrightRed().a(message).reset());
+    }
+
+    private static void printError(String message) {
+        printError(message, true);
+    }
+
+    private static void printSuccess(String message) {
+        System.out.println(ansi().fgBrightGreen().a(message).reset());
     }
 }
